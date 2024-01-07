4367b8aff5a117aef6e7d536cb0c6095ea0d06f2
Will <71606954+willbert03@users.noreply.github.com>
2024-01-07 19:22:29 +0000

    added popup towards portfolios

---
+++ popup.js
@@ -0,0 +1,9 @@
+// Function to open the popup
+function openPopup() {
+  document.getElementById('portfolioPopup').style.display = 'block';
+}
+
+// Function to close the popup
+function closePopup() {
+  document.getElementById('portfolioPopup').style.display = 'none';
+}
