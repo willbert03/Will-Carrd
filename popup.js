fea14273fe10046254f34a5624518276989ea785
Will <71606954+willbert03@users.noreply.github.com>
2024-01-08 20:51:21 +0000

    Updated portfolio related stuff

--- popup.js
+++ popup.js
@@ -1,9 +1,23 @@
 // Function to open the popup
 function openPopup() {
-  document.getElementById('portfolioPopup').style.display = 'block';
+  document.getElementById("portfolioPopup").style.display = "block";
 }
 
 // Function to close the popup
 function closePopup() {
-  document.getElementById('portfolioPopup').style.display = 'none';
+  document.getElementById("portfolioPopup").style.display = "none";
 }
+
+// Hides when press Escape
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    closePopup();
+  }
+});
+
+// Hide when android back button pressed
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("popstate", function (event) {
+    closePopup();
+  });
+});
