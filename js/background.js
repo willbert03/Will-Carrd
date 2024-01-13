f251bcad8391fddc96b110640e60286f97f399e7
Will <71606954+willbert03@users.noreply.github.com>
2024-01-13 22:11:54 +0000

    Having real background issues

--- js/background.js
+++ js/background.js
@@ -1,5 +1,5 @@
       // Array of image filenames
-      const imageArray = ["img/cover/DS_1.jpg", "img/cover/DS_2.jpg", "img/cover/DS_3.jpg", "img/cover/DS_4.jpg", "img/cover/DS_5.jpg", "img/cover/DS_6.jpg", "img/cover/DS_7.jpg", "img/cover/DS_8.jpg", "img/cover/DS_9.jpg", "img/cover/Giraffe.png", "img/cover/Greenbow.png", "img/cover/LondonS.png", "img/cover/LondonW.png", "img/cover/Lorikeet.png", "img/cover/Mountains.png", "img/cover/Paint.png", "img/cover/Penquin.png", "img/cover/Rhino.png", "img/cover/Swim.png" ];
+      const imageArray = ["img/Patterns/Rhino.svg", "img/Patterns/Giraffe.svg", "img/Patterns/Lorikeet.svg", "img/Patterns/Pattern.svg", ];
 
       // Get the image element by its ID
       const rotatingImage = document.getElementById("rotatingImage");
