585d9b7b05d29e5b5d9be4ea99f64875059292fe
Will <71606954+willbert03@users.noreply.github.com>
2024-03-01 21:41:38 +0000

    added patterns and better css

--- js/background.js
+++ js/background.js
@@ -1,5 +1,5 @@
       // Array of image filenames
-      const imageArray = ["img/SMPTE.png", "img/Patterns/Rhino.svg", "img/Patterns/Giraffe.svg", "img/Patterns/Lorikeet.svg", "img/Patterns/Pattern.svg", "img/Patterns/Animals.svg"];
+      const imageArray = ["img/SMPTE.png", "img/Patterns/Animals.svg", "img/Patterns/Animals2.svg", "img/Patterns/Animals3.svg", "img/Patterns/Animals4.svg", "img/Patterns/Animals5.svg", "img/Patterns/Animals6.svg", "img/Patterns/Circles.svg", "img/Patterns/Giraffes.svg", "img/Patterns/Giraffes2.svg", "img/Patterns/Giraffes3.svg", "img/Patterns/Lorikeet.svg", "img/Patterns/Lorikeet2.svg", "img/Patterns/Lorikeet3.svg", "img/Patterns/Rhinos.svg", "img/Patterns/Rhinos2.svg", "img/Patterns/RhinoWalk.svg", "img/Patterns/Triangles.svg", ];
 
       // Get the image element by its ID
       const rotatingImage = document.getElementById("rotatingImage");
