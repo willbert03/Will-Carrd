3f6ca0cfb38de98dc653c3ce1b8d42e8d0e82950
Will <71606954+willbert03@users.noreply.github.com>
2024-03-01 17:18:00 +0000

    Google Analytics

--- js/GAL.js
+++
@@ -1,9 +0,0 @@
-    <!-- Google tag (gtag.js) -->
-      async
-      src="https://www.googletagmanager.com/gtag/js?id=G-E2Y68B4DEL"
-      window.dataLayer = window.dataLayer || [];
-      function gtag() {
-        dataLayer.push(arguments);
-      }
-      gtag("js", new Date());
-      gtag("config", "G-E2Y68B4DEL");
