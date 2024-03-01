aaf3343aa1f265f25b4487d8210216c4795f6955
Will <71606954+willbert03@users.noreply.github.com>
2024-03-01 17:07:49 +0000

    Updated images & analytics

---
+++ js/GAL.js
@@ -0,0 +1,9 @@
+    <!-- Google tag (gtag.js) -->
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-E2Y68B4DEL"
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+      gtag("config", "G-E2Y68B4DEL");
